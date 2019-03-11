33d209adb4f6ff338aea018aa874ef681ec11a50
Daniele Genta <s236676@studenti.polito.it>
2019-03-11 13:52:16 +0100

    Classe voto.

--- src/it/polito/tdp/libretto/Voto.java
+++ src/it/polito/tdp/libretto/Voto.java
@@ -3,9 +3,51 @@ import java.time.LocalDate;
 
 public class Voto
 {
+	public Voto(int voto, String corso, LocalDate data) {
+		super();
+		this.voto = voto;
+		this.corso = corso;
+		this.data = data;
+	}
+	
 	private int voto;
+	public int getVoto() {
+		return voto;
+	}
+
+
+	public void setVoto(int voto) {
+		this.voto = voto;
+	}
+
+
+	public String getCorso() {
+		return corso;
+	}
+
+
+	public void setCorso(String corso) {
+		this.corso = corso;
+	}
+
+
+	public LocalDate getData() {
+		return data;
+	}
+
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+
 	private String corso;
 	private LocalDate data; //alternative obsolete: date, calendar
+	
+	
+
+	
+	
 	
 	
 }
